975c9035a798f92ee1d60b1a79e2101820d8e5aa
Chris Augier <chris.augier@gmail.com>
2017-09-29 22:17:47 +0100

    Add basis for particles to interact with each other
    Adding method to tell if surface area is overlapping with another
    particle
    Colour each particle red if interacted

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,12 +5,16 @@ void ofApp::setup(){
     // set the background to black for max contrast
     ofBackground(0);
 
-    const int NUM_PARTICLES = 100;
+    const int NUM_PARTICLES = 10;
 
     for(int i=0; i<NUM_PARTICLES; i++){
         Particle particle;
         particle.setup();
         particles.push_back(particle);
+    }
+
+    for(int i=0; i<particles.size(); i++){
+        particles[i].setParticles(&particles);
     }
 }
 
@@ -33,15 +37,15 @@ void Particle::setup(){
     // assign a randomised radius
     // we assume this is 'infinitesimally small' and therefore
     // the ignore the actual radius except for visualising
-    radius = 3;
+    radius = 20;
 
     // assign it's starting position
     pos.x = ofRandom(radius, ofGetWidth() - radius);
     pos.y = ofRandom(radius, ofGetHeight() - radius);
 
     // assign it's velocity
-    velocity.x = ofRandom(-10, 10);
-    velocity.y = ofRandom(-10, 10);
+    velocity.x = ofRandom(-5, 5);
+    velocity.y = ofRandom(-5, 5);
 
     // give it a randomised colour
     colour.r = ofRandom(0, 255);
@@ -78,10 +82,46 @@ void Particle::update(){
     if (pos.y > maxY || pos.y < minY){
         velocity *= reverseY;
     }
+
+    // TODO think about what if they interact at a wall?
+
+    for(int i=0; i<particles->size(); i++){
+        // check that the particle isn't this particle
+        // (is there a better way of doing this?)
+        if (pos.x == particles->at(i).pos.x &&
+            pos.y == particles->at(i).pos.y){
+            continue;
+        }
+
+        if (isInteracting(&particles->at(i))){
+            colour = ofColor(255,0,0);
+        }
+    }
 }
 
 // draw the particle (the easy bit!)
 void Particle::draw(){
     ofSetColor(colour);
     ofDrawCircle(pos.x, pos.y, radius);
+}
+
+void Particle::setParticles(vector<Particle> *p){
+    particles = p;
+}
+
+// in order to tell if a particle interacts with another one
+// their areas must be overlapping. The simplest way to calculate
+// this is to see if the distance between the two points is
+// smaller than the two particles radii combined.
+bool Particle::isInteracting(Particle * particle){
+    // calculate the distance between two particles
+    float dx = pos.x - particle->pos.x;
+    float dy = pos.y - particle->pos.y;
+
+    float d = sqrt(pow(dx, 2) + pow(dy,2));
+
+    if (d < radius + particle->radius){
+        return true;
+    }
+    return false;
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -7,11 +7,16 @@ public:
     void setup();
     void update();
     void draw();
-    
+
+    void setParticles(vector<Particle> * particles);
+    bool isInteracting(Particle * particle);
+
     ofVec2f pos;
     ofVec2f velocity;
     ofColor colour;
     float radius;
+
+    vector<Particle> * particles;
 };
 
 class ofApp : public ofBaseApp{
@@ -20,6 +25,7 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+
 
         vector<Particle> particles;
 };
