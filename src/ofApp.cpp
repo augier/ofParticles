1becc7dfcc1b22c00cc94bc5eabe7976bdcf8a7c
Chris Augier <chris.augier@gmail.com>
2017-10-06 23:24:24 +0100

    Updating so isInteracting is using distance function

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -115,10 +115,7 @@ void Particle::setParticles(vector<Particle> *p){
 // smaller than the two particles radii combined.
 bool Particle::isInteracting(Particle * particle){
     // calculate the distance between two particles
-    float dx = pos.x - particle->pos.x;
-    float dy = pos.y - particle->pos.y;
-
-    float d = sqrt(pow(dx, 2) + pow(dy,2));
+    float d = pos.distance(particle->pos);
 
     if (d < radius + particle->radius){
         return true;
